01ab54f5a5b563b9f83bdde43bd4fb2128b2694c
Albert Corsten <corstena@uw.edu>
2016-10-10 16:50:43 -0700

    Added optionals and test cases

--- CalcArrayPoints/main.swift
+++ CalcArrayPoints/main.swift
@@ -5,113 +5,189 @@
 //  Created by Albert on 10/7/16.
 //  Copyright © 2016 iSchool. All rights reserved.
 //
+//  Optional extra credit DONE
 
 import Foundation
 
-func add(valueOne : Int, valueTwo : Int) -> Double {
-    return Double(valueOne + valueTwo)
-}
-
-func subtract(valueOne : Int, valueTwo : Int) -> Double {
-    return Double(valueOne - valueTwo)
-}
-
-func multiply(valueOne : Int, valueTwo : Int) -> Double {
-    return Double(valueOne * valueTwo)
-}
-
-func divide(valueOne : Int, valueTwo: Int) -> Double {
-    return Double(valueOne) / Double(valueTwo)
-}
-
-func mathOperation(valueOne : Int, valueTwo : Int, operation : (Int, Int) -> Double) -> Double {
-    return operation(valueOne, valueTwo)
-}
-
-func arrayAdd(array : [Int]) -> Double {
-    var total = 0;
-    for value in array {
-        total += value
+func add(left : Int?, right : Int?) -> Double? {
+    if(left != nil && right != nil) {
+        return Double(left! + right!)
     }
-    return Double(total)
+    return nil
 }
 
-func arrayMultiply(array : [Int]) -> Double {
-    if array.count > 1 {
-        var total = array[0]
-        for index in 1...array.count - 1 {
-            total *= array[index]
+func subtract(left : Int?, right : Int?) -> Double? {
+    if(left != nil && right != nil) {
+        return Double(left! - right!)
+    }
+    return nil
+}
+
+func multiply(left : Int?, right : Int?) -> Double? {
+    if(left != nil && right != nil) {
+        return Double(left! * right!)
+    }
+    return nil
+}
+
+func divide(left : Int?, right: Int?) -> Double? {
+    if(left != nil && right != nil) {
+        return Double(left!) / Double(right!)
+    }
+    return nil
+}
+
+func mathOperation(left : Int?, right : Int?, operation : (Int?, Int?) -> Double?) -> Double? {
+    if(left != nil && right != nil) {
+        return operation(left!, right!)
+    }
+    return nil
+}
+
+func add(array : [Int]?) -> Double? {
+    if(array != nil) {
+        var total = 0;
+        for value in array! {
+            total += value
         }
         return Double(total)
-    } else {
-        print("Please enter a valid array")
-        return 0.0
     }
+    return nil
 }
 
-func arrayCount(array : [Int]) -> Double {
-    return Double(array.count)
+func multiply(array : [Int]?) -> Double? {
+    if(array != nil) {
+        if (array?.count)! > 1 {
+            var total = array?[0]
+            for index in 1...(array?.count)! - 1 {
+                total! *= (array?[index])!
+            }
+            return Double(total!)
+        } else {
+            print("Please enter a valid array")
+            return 0.0
+        }
+    }
+    return nil
 }
 
-func arrayAvg(array : [Int]) -> Double {
-    return arrayAdd(array: array) / Double(array.count)
+func count(array : [Int]?) -> Double? {
+    if(array != nil) {
+        return Double((array?.count)!)
+    }
+    return nil
 }
 
-func arrayOperation(array : [Int], operation : ([Int]) -> Double) -> Double {
-    return operation(array)
+func average(array : [Int]?) -> Double? {
+    if(array != nil) {
+       return add(array: array)! / Double((array?.count)!)
+    }
+    return nil
 }
 
-func addCoordinates (coordinate1 : (Int, Int), coordinate2 : (Int, Int)) -> (Int, Int) {
-    return (coordinate1.0 + coordinate2.0, coordinate1.1 + coordinate2.1)
+func reduce(array : [Int]?, operation : ([Int]?) -> Double?) -> Double? {
+    if array != nil {
+        return operation(array)
+    }
+    return nil
 }
 
-func subtractCoordinates (coordinate1 : (Int, Int), coordinate2 : (Int, Int)) -> (Int, Int) {
-    return (coordinate1.0 - coordinate2.0, coordinate1.1 - coordinate2.1)
+func add (p1 : (Int, Int), p2 : (Int, Int)) -> (Int, Int) {
+    return (p1.0 + p2.0, p1.1 + p2.1)
 }
 
-func addDictCoordinates (coordinateDictionary1 : [String : Double], coordinateDictionary2 : [String : Double]) -> [String : Double] {
+func subtract (p1 : (Int, Int), p2 : (Int, Int)) -> (Int, Int) {
+    return (p1.0 - p2.0, p1.1 - p2.1)
+}
+
+func add (p1 : [String : Int]?, p2 : [String : Int]?) -> [String : Int]? {
+    var totalx = 0
+    var totaly = 0
+    if(p1 != nil && p2 != nil) {
+        for (key, value) in p1! {
+            if(key == "x") {
+                totalx += value
+            } else if(key == "y") {
+                totaly += value
+            }
+        }
+        for (key, value) in p2! {
+            if(key == "x") {
+                totalx += value
+            } else if(key == "y") {
+                totaly += value
+            }
+        }
+        return ["x" : totalx, "y" : totaly]
+    }
+    return nil
+}
+
+func add (p1 : [String : Double]?, p2 : [String : Double]?) -> [String : Double]? {
     var totalx = 0.0
     var totaly = 0.0
-    for (key, value) in coordinateDictionary1 {
-        if(key == "x") {
-            totalx += value
-        } else if(key == "y") {
-            totaly += value
+    if(p1 != nil && p2 != nil) {
+        for (key, value) in p1! {
+            if(key == "x") {
+                totalx += value
+            } else if(key == "y") {
+                totaly += value
+            }
         }
-    }
-    for (key, value) in coordinateDictionary2 {
-        if(key == "x") {
-            totalx += value
-        } else if(key == "y") {
-            totaly += value
+        for (key, value) in p2! {
+            if(key == "x") {
+                totalx += value
+            } else if(key == "y") {
+                totaly += value
+            }
         }
+        return ["x" : totalx, "y" : totaly]
     }
-    return ["x" : totalx, "y" : totaly]
+    return nil
 }
 
-func subtractDictCoordinates (coordinateDictionary1 : [String : Double], coordinateDictionary2 : [String : Double]) -> [String : Double] {
+func subtract (p1 : [String : Int]?, p2 : [String : Int]?) -> [String : Int]? {
+    var totalx = 0
+    var totaly = 0
+    if(p1 != nil && p2 != nil) {
+        for (key, value) in p1! {
+            if(key == "x") {
+                totalx -= value
+            } else if(key == "y") {
+                totaly -= value
+            }
+        }
+        for (key, value) in p2! {
+            if(key == "x") {
+                totalx -= value
+            } else if(key == "y") {
+                totaly -= value
+            }
+        }
+        return ["x" : totalx, "y" : totaly]
+    }
+    return nil
+}
+
+func subtract (p1 : [String : Double]?, p2 : [String : Double]?) -> [String : Double]? {
     var totalx = 0.0
     var totaly = 0.0
-    for (key, value) in coordinateDictionary1 {
-        if(key == "x") {
-            totalx -= value
-        } else if(key == "y") {
-            totaly -= value
+    if(p1 != nil && p2 != nil) {
+        for (key, value) in p1! {
+            if(key == "x") {
+                totalx -= value
+            } else if(key == "y") {
+                totaly -= value
+            }
         }
-    }
-    for (key, value) in coordinateDictionary2 {
-        if(key == "x") {
-            totalx -= value
-        } else if(key == "y") {
-            totaly -= value
+        for (key, value) in p2! {
+            if(key == "x") {
+                totalx -= value
+            } else if(key == "y") {
+                totaly -= value
+            }
         }
+        return ["x" : totalx, "y" : totaly]
     }
-    return ["x" : totalx, "y" : totaly]
+    return nil
 }
-
-
-print(mathOperation(valueOne: 1, valueTwo: 2, operation: divide))
-print(arrayOperation(array : [1, 2, 3, 4 ,5], operation : arrayAvg))
-
-var explore = [5.5 : "StringValue"]
-print("Can we key by doubles? \(explore[5.5]!)")
